63b3107bd87fd7bc4d4fd8b57b1b910182d4a099
Rydge Craker <rcraker5@student.cvtc.edu>
2018-02-14 22:13:50 +0000

    Created basic Asteroids file and loaded js properly

--- app/assets/javascripts/Asteroids.js
+++ app/assets/javascripts/Asteroids.js
@@ -1,1 +1,24 @@
-alert("Here");
+/* global Phaser */
+
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', {
+    preload: preload, create: create, update: update, render: render
+});
+
+function preload() {
+    //Load sprites and images
+    
+}
+
+function create(){
+    //Game varaibles set up
+    
+}
+
+function update(){
+    //Updates 60 times a second
+    
+}
+
+function render() {
+    //For rendering
+}
