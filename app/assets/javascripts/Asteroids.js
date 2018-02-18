c7538ce623f0ed406911ef5f653f4af2bfa7deb0
Rydge Craker <rcraker5@student.cvtc.edu>
2018-02-18 22:26:01 +0000

    Added LOTS of collision detection methods for detecting collisions between polygons

--- app/assets/javascripts/Asteroids.js
+++ app/assets/javascripts/Asteroids.js
@@ -4,6 +4,9 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', {
 });
 
 var spaceShip;
+var asteroids = [];
+var bullets = [];
+var powerups = [];
 
 function preload() {
     //Load sprites and images
@@ -86,10 +89,8 @@ function create(){
     game.physics.enable(spaceShip, Phaser.Physics.ARCADE);
 
     //Makes it so the sprint doesn't go really fast and slows down
-    
-        console.log('body stuff');
-        spaceShip.body.drag.set(70);
-        spaceShip.body.maxVelocity.set(200);
+    spaceShip.body.drag.set(70);
+    spaceShip.body.maxVelocity.set(200);
    
 
     //Creates a property that allows for arrowKey movement.
@@ -166,7 +167,163 @@ function update(){
     }
 
     game.world.wrap(spaceShip, 16);
-  
+    checkCollisions();
+}
+
+function checkCollisions(){
+    checkPlayerColls();
+    checkBulletColls();
+}
+
+
+//A function that checks to see if the player collides with asteroids or powerups.
+function checkPlayerColls(){
+    if(checkPlayerCollideAsteroid()){
+        //Player collided with an asteroid
+    }
+    
+    if(checkPlayerCollidePowerup()){
+        //Player collided with powerup
+    }
+}
+
+function checkPlayerCollideAsteroid(){
+    for(var i = 0; i < this.asteroids.length; i++){
+        if(doesPlayerCollideWithAsteroid(this.asteroids[i])){
+            //Player collided with asteroid at [i]
+            return true;
+        }
+    }    
+    return false;
+}
+
+function doesPlayerCollideWithAsteroid(aster){
+    var playerPoints = [new Point(this.spaceShip.x, this.spaceShip.y), new Point(this.spaceShip.x + this.spaceShip.width, this.spaceShip.y), new Point(this.spaceShip.x + this.spaceShip.width, this.spaceShip.y + this.spaceShip.height), new Point(this.spaceShip.x, this.spaceShip.y + this.spaceShip.height)];
+    return polygonIntersectsPolygon(aster.points, playerPoints);
+}
+
+function polygonIntersectsPolygon(poly1, poly2) {
+    for(var i = 0; i < poly1.length; i++){
+        if(polyContainsPoint(poly1[i], poly2)){
+            return true;
+        }
+    }
+    return false;
+}
+
+function polyContainsPoint(point, poly){
+    var maxAndMinPoints = getMaxAndMinPolygon(poly);
+    
+    if (!polyContainsQuickCheck(point, maxAndMinPoints)) {
+			return false;
+	}
+	
+	var ray = getPolyRay(maxAndMinPoints[0].x, maxAndMinPoints[1].x, point.x, point.y);
+	var polySides = getSidesOfPolygon(poly);
+	
+	return colDetectUsingRaycasting(ray, polySides);
+}
+
+function colDetectUsingRaycasting(ray, sides){
+    	var intersections = 0;
+		for (var i = 0; i < sides.length; i++) {
+			if (doLinesIntersect(ray, sides[i])) {
+				intersections++;
+			}
+		}
+		if ((intersections & 1) === 1) {
+			return true; // Point is inside polygon
+		}
+		return false;	 // Point is outside polygon
+}
+
+function doLinesIntersect(ray, side){
+    var det, gamma, lambda;
+    det = (ray.point2.x - ray.point1.x) * (side.point2.y - side.point1.y) - (side.point2.x - side.point1.x) * (ray.point2.y - ray.point1.y);
+    if (det === 0) {
+        return false;
+    }
+    lambda = ((side.point2.y - side.point1.y) * (side.point2.x - ray.point1.x) + (side.point1.x - side.point2.x) * (side.point2.y - ray.point1.y)) / det;
+    gamma = ((ray.point1.y - ray.point2.y) * (side.point2.x - ray.point1.x) + (ray.point2.x - ray.point1.x) * (side.point2.y - ray.point1.y)) / det;
+    return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
+}
+
+function getSidesOfPolygon(poly){
+	var sides = [];
+	
+	for (var i = 0; i < poly.length; i++) {
+		if (i !== poly.length - 1) {
+			sides[i] = new CollLine(new Point(poly[i].x, poly[i].y), new Point(poly[i + 1].x, poly[i + 1].y));
+		} else {
+			sides[i] = new CollLine(new Point(poly[i].x, poly[i].y), new Point(poly[0].x, poly[0].y));
+		}
+	}
+	
+	return sides;
+}
+
+function getPolyRay(maxX, minX, pointX, pointY){
+    var epsilon = (maxX - minX) / 100;
+	return new CollLine(new Point(minX - epsilon, pointY), new Point(pointX, pointY));
+}
+
+function CollLine(point1, point2){
+    this.point1 = point1;
+    this.point2 = point2;
+}
+
+function polyContainsQuickCheck(point, maxAndMins){
+    if (point.x > maxAndMins[0].x || point.x < maxAndMins[1].x || point.y > maxAndMins[0].y || point.y < maxAndMins[1].y) {
+		return false;
+	}
+	return true;
+}
+
+function getMaxAndMinPolygon(poly){
+    
+    var maxAndMins = [];
+    
+	maxAndMins[0] = poly[0].x; // maxX
+	maxAndMins[1] = poly[0].x; // minX
+	maxAndMins[2] = poly[0].y; // maxY
+	maxAndMins[3] = poly[0].y; // minY
+	
+	var iterator = 0;
+	
+	for (var i = 1; i < poly.length; i++) {
+		iterator = poly[i].x;
+		if (maxAndMins[0] < iterator) {
+			maxAndMins[0] = iterator;
+		}
+		
+		if (maxAndMins[1] > iterator) {
+			maxAndMins[1] = iterator;
+		}
+	}
+	for (i = 1; i < poly.length; i++) {
+		iterator = poly[i].y;
+		if (maxAndMins[2] < iterator) {
+			maxAndMins[2] = iterator;
+		}
+		if (maxAndMins[3] > iterator) {
+			maxAndMins[3] = iterator;
+		}
+	}
+	
+	
+	//Max X and Y are stored in point [0], Min X and Y are stored in point [1]
+	return [new Point(maxAndMins[0], maxAndMins[2]), new Point(maxAndMins[1], maxAndMins[3])];
+}
+
+
+function checkPlayerCollidePowerup(){
+    
+    return false;
+}
+
+//A function that checks to see if a bullet collides with an asteroid.
+function checkBulletColls(){
+    
 }
 
 function render() {
