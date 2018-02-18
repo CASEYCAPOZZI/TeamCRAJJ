0ba00bdb5ae9f6bffeac463b886252b596ef67a9
Rydge Craker <rcraker5@student.cvtc.edu>
2018-02-18 19:22:56 +0000

    I did a whole bunch of stuff: -Increased Max velocity to 500 -Changed rotation logic so that ship moves forward instead of left. -Changed pause key to esacape and rounted the spacebar to call an empty method called fireBullets -Added WASD support for movement. -Corrected errors in JS code by making global variables and such. -Changed ternary operator for pause to a more effecient line of code. -Created a Point object and an Asteroid object. -Created logic for randomized Asteroid types, sides, and sizes! WOO! -Created a function to paint asteroids to the screen and put a test-one up. -Commented all of the stuff I added/changed to help you guys understand what's going on (especially in the Asteroid constructor)

--- app/assets/javascripts/Asteroids.js
+++ app/assets/javascripts/Asteroids.js
@@ -3,25 +3,77 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', {
     preload: preload, create: create, update: update, render: render
 });
 
+var spaceShip;
+
 function preload() {
     //Load sprites and images
-    spaceShipImagePath = "/assets/player-2b769c18603d84592d2fb06ba6ae8ed0ddee574356e5a152717f541234278fde.png";
+    var spaceShipImagePath = "/assets/player-2b769c18603d84592d2fb06ba6ae8ed0ddee574356e5a152717f541234278fde.png";
     game.load.image("spaceShip", spaceShipImagePath);
    
 
-    var spaceShip;
 }
 
 
 
-function create(){
-    //Game variables set up
-/*    var player = Player();
-   
- 
-    game.input.keyboard.addKeyCapture([ Phaser.Keyboard.SPACEBAR ]); */
+//This function creates an Asteroid object. To create one you must specify a few things:
+//
+//xLoc and yLoc: the center point of the asteroid.
+//minDistance: the smallest number of pixels you want an edge of the asteroid to get to the center point
+//maxDistance: the largest number of pixels you want an edge of the asteroid to get from the center point
+//numSides: the number of sides you want the asteroid to have. MUST be a minimum of 3
+function Asteroid(xLoc, yLoc, minDistance, maxDistance, numSides) {
+    
+    //Creates some properties of the Asteroid Object
+    //
+    //The Center point, obviously.
+    this.centerPoint = new Point(xLoc, yLoc);
+    
+    //An array that stores all of the verticies of the asteroid (end points of line segments)
+    this.points = [];
+    
+    //min and max distance.
+    this.minDistance = minDistance;
+    this.maxDistance = maxDistance;
+    
+    //An array that stores the Phaser "line" objects inside it for drawing and logic purposes.
+    this.lines = [];
+    
+    //Determine what the angle between each point (relative to the center point) must be based on the number of sides.
+    var anglePerPoint = (2 * Math.PI) / numSides;
+    
+    //Some clever math to create randomized points that are within the constraints we gave. 
+    for(var i = 0; i < numSides; i++){
+        //The starting angle for the point is determined by which point it is.
+        var currentAngle = i * anglePerPoint;
+        //Create a random vector (a point in space with a length) using our min/max distances.
+        var randomVector = Math.floor(Math.random() * (maxDistance - minDistance)) + minDistance; 
+        
+        //Extract the vector we created into x and y shifts using a little trig.
+        var xLocation = this.centerPoint.x + (randomVector * Math.cos(currentAngle));
+        var yLocation = this.centerPoint.y + (randomVector * Math.sin(currentAngle));
+        
+        //Add a new point to the points array using the locations we just generated.
+        this.points[i] = new Point(xLocation, yLocation);
+    }
+    
+    
+    //Use the points to create line segments to fill up the line array.
+    for(i = 0; i < this.points.length; i++){
+        //Because the every point must be used twice (once as the beginning of a line segment, and once as the end)
+        //We have to make sure we don't an array out of bounds error when looping around to use the first point twice.
+        if(i < this.points.length - 1){
+            //Create a line with the given points.
+            this.lines[i] = new Phaser.Line(this.points[i].x, this.points[i].y, this.points[i+1].x, this.points[i+1].y);
+        } else {
+            //Create a line with the given points.
+            this.lines[i] = new Phaser.Line(this.points[i].x, this.points[i].y, this.points[0].x, this.points[0].y);
+        }
+    }
+    
+}
 
-    cursors = game.input.keyboard.createCursorKeys();
+
+function create(){
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     
@@ -39,42 +91,77 @@ function create(){
         spaceShip.body.drag.set(70);
         spaceShip.body.maxVelocity.set(200);
    
+
+    //Creates a property that allows for arrowKey movement.
+    this.cursors = game.input.keyboard.createCursorKeys();
+    
+    //Creates a property to allow for WASD movement along with arrow keys.
+    this.wasd = {
+      up: game.input.keyboard.addKey(Phaser.Keyboard.W),
+      down: game.input.keyboard.addKey(Phaser.Keyboard.S),
+      left: game.input.keyboard.addKey(Phaser.Keyboard.A),
+      right: game.input.keyboard.addKey(Phaser.Keyboard.D),
+    };
+    
+    //Creates a property that watches the space bar.
+    this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    //Creates a property that watches the escape key.
+    this.escapeKey = game.input.keyboard.addKey(Phaser.Keyboard.ESC);
+    
+    //Sets the "togglePause" method to be called when the escape key is pressed.
+    this.escapeKey.onDown.add(togglePause, this);
+    
+    //Set the "fireBullet" method to be called when the space bar is pressed.
+    this.spaceKey.onDown.add(fireBullet, this);
+    
+    
+    
+    
+    //This is just temporary. Once everyone undertands what it's doing, we'll
+    //actually create an array of astroids and fill it as we go along.
+    //In this example, though. I'm creating an asteroid centered at the point 300, 200.
+    //It's got a minimum vertex height of 10px and a max vertex height of 100.
+    //I also made it have 10 sides.
+    //You can see the generator in action by refreshing the page a few times
+    //after you load the game. On each refresh, the asteroid will be generated differently.
+    this.asteroid = new Asteroid(300, 200, 10, 100, 10);
+    
     
 
+}
 
-   
 
-    //Allows keyboard controls so you can move and pause the game( arrow keys and space bar.)
-    cursors = game.input.keyboard.createCursorKeys();
-    spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    spaceKey.onDown.add(togglePause, this);
+//An object that allows us to store points in one variable.
+function Point(x, y){
+    this.x = x;
+    this.y = y;
 }
 
 function togglePause() {
-    game.physics.arcade.isPaused = (game.physics.arcade.isPaused) ? false : true;
+    game.physics.arcade.isPaused = !game.physics.arcade.isPaused;
+}
+
+function fireBullet() {
+    
 }
 
 function update(){
-    //Below is the logic for the keys to turn and what not.
-    if (cursors.up.isDown)
-    {
-        game.physics.arcade.accelerationFromRotation(spaceShip.rotation, 300, spaceShip.body.acceleration);
-    }
-    else
-    {
+    
+    //Pressing UpArrow or W
+    if (this.cursors.up.isDown || this.wasd.up.isDown) {
+        game.physics.arcade.accelerationFromRotation(spaceShip.rotation - (Math.PI / 2.0), 300, spaceShip.body.acceleration);
+    } else {
         spaceShip.body.acceleration.set(0);
     }
 
-    if (cursors.left.isDown)
-    {
+    //Pressing LeftArrow or A
+    if (this.cursors.left.isDown || this.wasd.left.isDown) {
         spaceShip.body.angularVelocity = -300;
-    }
-    else if (cursors.right.isDown)
-    {
+        
+    //Pressing RightArrow or D
+    } else if (this.cursors.right.isDown || this.wasd.right.isDown) {
         spaceShip.body.angularVelocity = 300;
-    }
-    else
-    {
+    } else {
         spaceShip.body.angularVelocity = 0;
     }
 
@@ -83,30 +170,14 @@ function update(){
 }
 
 function render() {
-  
+    //Painting the example asteroid.
+    paintAsteroid(this.asteroid);
 }
 
-
-/// Objects
-
-/*
-function Player() {
-    this.player = game.add.sprite(this.game.world.centerX, this.game.world.centerY, "player");
-    this.player.anchor.set(0.5);
-    this.player.scale.y = 1;
+//A function that takes in an Asteroid object and paints all of the lines within it.
+function paintAsteroid(asteroid){
+    for(var i = 0; i < asteroid.lines.length; i++){
+        game.debug.geom(asteroid.lines[i], 'rgba(255,255,255,1)');
+    }
 }
-
-function Asteroid(xLocation, yLocation){
-    this.asteroid = game.add.sprite(xLocation, yLocation, "asteroid");
-    this.asteroid.anchor.set(0.5);
-    this.asteroid.scale.y = 1;
-}
-
-function Bullet(xLocation, yLocation){
-    this.asteroid = game.add.sprite(xLocation, yLocation, "bullet");
-    this.asteroid.anchor.set(0.5);
-    this.asteroid.scale.y = 1;
-}
-
-*/
 
