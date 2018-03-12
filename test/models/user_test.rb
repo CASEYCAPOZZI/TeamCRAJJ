a204a9dde3c3d58ac290adaf81ea0aafbba8f737
Austin R Kaiser <austinkaiser1212@gmail.com>
2018-03-12 21:54:58 +0000

    Add test to find user in database

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,10 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  def test_find_user
+    # Asserts that the controller really put the book in the database.
+    assert_not_nil User.find_by(name: "Test User")
+  end 
+  
 end
