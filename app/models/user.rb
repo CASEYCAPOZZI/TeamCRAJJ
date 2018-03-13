6a1a0d326dbed713f215de9450ba8aed196fca16
Austin R Kaiser <austinkaiser1212@gmail.com>
2018-03-13 20:14:59 +0000

    Add passing name validation test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  validates :name, presence: true
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
